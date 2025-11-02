2662bfff7b289dadde0b6952915a7cf8fca711c3
leander@kafemann.berlin <leander@kafemann.berlin>
2025-11-02 15:49:01 +0100

    add setup wizard

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-import os, random
+import os, random, sqlite3
 import pyautogui as py
 
 if not "password.txt" in os.listdir():
@@ -6,4 +6,15 @@ if not "password.txt" in os.listdir():
         pwd_ = int(py.password(text="Set your password:", title="Password Setup"))
         random.seed(pwd_)
         f.write(str(random.randint(0, 1000000000)))
+
+con = sqlite3.connect("database.db")
+cur = con.cursor()
+cur.execute("CREATE TABLE Defaultroom (name VARCHAR PRIMARY KEY, dates VARCHAR);")
+cur.execute("INSERT INTO Defaultroom (name, dates) VALUES ('Default', '1.1.2000');")
+con.commit()
+con.close()
+
+with open("rooms.txt", "w", encoding="utf-8") as f:
+    f.write("defaultroom")
+
 py.alert("Setup completed! Thanks for using Setup Wizard for GoodFoodServer 1.0", "Completed")
